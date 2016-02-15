1436e938cff10204c40294b7048b1db278dbc8c3
Richard W. Eggert II <richard.eggert@gmail.com>
2016-02-14 21:49:39 -0500

    corrected MathemagicsProperties.minimum

--- src/test/scala/io/github/reggert/scalademo/scalaexamples/MathemagicsProperties.scala
+++ src/test/scala/io/github/reggert/scalademo/scalaexamples/MathemagicsProperties.scala
@@ -1,15 +1,16 @@
 package io.github.reggert.scalademo.scalaexamples
 
-import org.scalacheck.{Prop, Properties}
-
-import Prop._
+import org.scalacheck._
 
 
 object MathemagicsProperties extends Properties("Mathemagics") {
+  import Prop._
 
   property("minimum returns a value that is <= all elements in list") = forAll {list : List[Int] =>
-    val result = Mathemagics.minimum(list)
-    list.forall(result <= _)
+    list.nonEmpty ==> {
+      val result = Mathemagics.minimum(list)
+      list.forall(result <= _)
+    }
   }
 
 }
